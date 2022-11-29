a402117eb3705668380054a39f671afbf7508953
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-11-29 23:39:35 +0100

    misc documenting

--- java/src/main/java/palaiologos/scijava/MathContext.java
+++ java/src/main/java/palaiologos/scijava/MathContext.java
@@ -1,9 +1,40 @@
+/*
+    scijava
+    Copyright (C) 2022 Kamila Szewczyk
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package palaiologos.scijava;
 
+/**
+ * A record encapsulating the context settings which describe rules for numerical
+ * operations, such as those implemented by {@link SciFloat}.
+ *
+ * The settings are:
+ * <ul>
+ *     <li>precision: the number of <b>binary</b> digits not smaller than one to be used for an operation's result</li>
+ *     <li>rounding mode: the algorithm to be used for rounding</li>
+ * </ul>
+ *
+ * @author Kamila Szewczyk
+ * @see SciFloat
+ */
 public record MathContext(int precision, palaiologos.scijava.MathContext.RoundingMode roundingMode) {
     public MathContext {
-        if (precision < 0) {
-            throw new IllegalArgumentException("precision < 0");
+        if (precision < 1) {
+            throw new IllegalArgumentException("precision < 1");
         }
         if (roundingMode == null) {
             throw new IllegalArgumentException("roundingMode == null");
@@ -15,5 +46,16 @@ public record MathContext(int precision, palaiologos.scijava.MathContext.Roundin
         DOWN,
         NEAREST,
         ZERO
+    }
+
+    /**
+     * @return the string representation of this {@code MathContext} object.
+     */
+    @Override
+    public String toString() {
+        return "MathContext{" +
+                "precision=" + precision +
+                ", roundingMode=" + roundingMode +
+                '}';
     }
 }
--- java/src/main/java/palaiologos/scijava/SciInteger.java
+++ java/src/main/java/palaiologos/scijava/SciInteger.java
@@ -27,6 +27,17 @@ import java.util.Objects;
 import static palaiologos.scijava.NativeLibrary.load;
 import static palaiologos.scijava.NativeLibrary.resourceName;
 
+/**
+ * Arbitrary precision integers provided by SciJava.
+ *
+ * <p>SciInteger provides methods that are similar to those in {@link java.math.BigInteger}, such
+ * as basic arithmetics, comparison, GCD/LCM, factorials, bit operations, primality-related utilities,
+ * fibonacci numbers, lucas numbers and integer factorisation using the Pollard-Rho algorithm.
+ *
+ * <p>SciInteger is backed by the GMP library.
+ *
+ * @author Kamila Szewczyk
+ */
 public final class SciInteger implements Comparable<SciInteger>, Cloneable {
     static {
         try {
@@ -80,7 +91,7 @@ public final class SciInteger implements Comparable<SciInteger>, Cloneable {
     private static native void setBit(long dest, long a, int b);
     private static native void clearBit(long dest, long a, int b);
     private static native void flipBit(long dest, long a, int b);
-    private static native void testBit(long dest, long a, int b);
+    private static native boolean testBit(long a, int b);
     private static native int bitCount(long a);
     private static native int bitLength(long a);
     private static native boolean isPrime(long a, int certainty);
@@ -102,199 +113,453 @@ public final class SciInteger implements Comparable<SciInteger>, Cloneable {
     private static native void copy(long dest, long src);
     private static native void factor(Map<SciInteger, SciInteger> destFactors, long a);
 
-    // Public API.
+    /**
+     * The SciInteger constant 0.
+     */
     public static final SciInteger ZERO = fromInteger(0);
+
+    /**
+     * The SciInteger constant 1.
+     */
     public static final SciInteger ONE = fromInteger(1);
+
+    /**
+     * The SciInteger constant 2.
+     */
     public static final SciInteger TWO = fromInteger(2);
+
+    /**
+     * The SciInteger constant 5.
+     */
     public static final SciInteger FIVE = fromInteger(5);
+
+    /**
+     * The SciInteger constant 10.
+     */
     public static final SciInteger TEN = fromInteger(10);
 
+    /**
+     * Return a new SciInteger with the value of the specified integer.
+     * @param i the integer to convert
+     * @return a new SciInteger instance
+     */
     public static SciInteger valueOf(int i) {
         return fromInteger(i);
     }
 
+    /**
+     * Return a new SciInteger with the value of the specified string.
+     * @param s the integer to convert
+     * @return a new SciInteger instance
+     * @throws ArithmeticException if the string does not contain a valid integer.
+     */
     public static SciInteger valueOf(String s) {
         return fromString(s);
     }
 
+    /**
+     * Return a new SciInteger with the value of the specified string.
+     * @param s the integer to convert
+     * @param radix the radix to use (2-36)
+     * @return a new SciInteger instance
+     * @throws ArithmeticException if the string does not contain a valid integer or radix is outside of range between 2 and 36.
+     */
     public static SciInteger valueOf(String s, int radix) {
         return fromStringRadix(s, radix);
     }
 
+    /**
+     * Add two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a + b
+     */
     public static SciInteger add(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         add(result.ptr, a.ptr, b.ptr);
         return result;
     }
-    
+
+    /**
+     * Subtract two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a - b
+     */
     public static SciInteger subtract(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         sub(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Multiply two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a * b
+     */
     public static SciInteger multiply(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         mul(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Divide two SciIntegers to produce a new SciInteger instance containing the quotient.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a / b
+     * @throws ArithmeticException if b is zero
+     */
     public static SciInteger divide(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         div(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Divide two SciIntegers to produce a new SciInteger instance containing the remainder.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a % b
+     * @throws ArithmeticException if b is zero
+     */
     public static SciInteger mod(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         rem(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Raise a SciInteger to a power to produce a new SciInteger instance.
+     * Does not modify the operand.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a ^ b
+     * @throws ArithmeticException if b < 0
+     */
     public static SciInteger pow(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         pow(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Negate a SciInteger to produce a new SciInteger instance.
+     * Does not modify the operand.
+     * @param a the operand
+     * @return a new SciInteger instance, the result of -a
+     */
     public static SciInteger negate(SciInteger a) {
         SciInteger result = SciInteger.fromInteger(0);
         negate(result.ptr, a.ptr);
         return result;
     }
 
+    /**
+     * Compute the absolute value of a SciInteger to produce a new SciInteger instance.
+     * Does not modify the operand.
+     * @param a the operand
+     * @return a new SciInteger instance, the result of |a|
+     */
     public static SciInteger abs(SciInteger a) {
         SciInteger result = SciInteger.fromInteger(0);
         abs(result.ptr, a.ptr);
         return result;
     }
 
+    /**
+     * Compute the greatest common divisor of two SciIntegers to produce a new SciInteger instance.
+     * Arguments may be negative or zero. Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of gcd(a, b)
+     */
     public static SciInteger gcd(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         gcd(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Compute the least common multiple of two SciIntegers to produce a new SciInteger instance.
+     * Arguments may be negative or zero. Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of lcm(a, b)
+     */
     public static SciInteger lcm(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         lcm(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Compute the factorial of a natural number to produce a new SciInteger instance.
+     * @param a the operand
+     * @return a new SciInteger instance, the result of a!
+     * @throws ArithmeticException if a < 0
+     */
     public static SciInteger factorial(int a) {
         SciInteger result = SciInteger.fromInteger(0);
         factorial(result.ptr, a);
         return result;
     }
 
+    /**
+     * Compute the sign of a SciInteger.
+     * @param a the operand
+     * @return -1 if a < 0, 0 if a = 0, 1 if a > 0
+     */
     public static SciInteger signum(SciInteger a) {
         SciInteger result = SciInteger.fromInteger(0);
         signum(result.ptr, a.ptr);
         return result;
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is less than b, false otherwise
+     */
     public boolean lt(SciInteger b) {
         return lt(ptr, b.ptr);
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is less than or equal to b, false otherwise
+     */
     public boolean lte(SciInteger b) {
         return lte(ptr, b.ptr);
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is greater than b, false otherwise
+     */
     public boolean gt(SciInteger b) {
         return gt(ptr, b.ptr);
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is greater than or equal to b, false otherwise
+     */
     public boolean gte(SciInteger b) {
         return gte(ptr, b.ptr);
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * Notice that this method always involves the underlying equality checks. When dealing
+     * with objects of unknown origin, consider using the {@link #equals(Object)} method first
+     * to avoid unnecessary equality checks in case the objects are physically equal or the
+     * types differ.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is equal to b, false otherwise
+     */
     public boolean eq(SciInteger b) {
         return eq(ptr, b.ptr);
     }
 
+    /**
+     * Compare this SciInteger with another SciInteger to determine their relative order.
+     * Notice that this method always involves the underlying equality checks. When dealing
+     * with objects of unknown origin, consider using the {@link #equals(Object)} method first
+     * to avoid unnecessary equality checks in case the objects are physically equal or the
+     * types differ.
+     * @param b the other SciInteger
+     * @return true if this SciInteger is not equal to b, false otherwise
+     */
     public boolean neq(SciInteger b) {
         return neq(ptr, b.ptr);
     }
 
+    /**
+     * Compute the value of the bit-wise "and" function of two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a & b
+     */
     public static SciInteger and(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         and(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Compute the value of the bit-wise "or" function of two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a | b
+     */
     public static SciInteger or(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         or(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Compute the value of the bit-wise "xor" function of two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a ^ b
+     */
     public static SciInteger xor(SciInteger a, SciInteger b) {
         SciInteger result = SciInteger.fromInteger(0);
         xor(result.ptr, a.ptr, b.ptr);
         return result;
     }
 
+    /**
+     * Compute the value of the bit-wise "not" function of a SciInteger to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the operand
+     * @return a new SciInteger instance, the result of ~a
+     */
     public static SciInteger not(SciInteger a) {
         SciInteger result = SciInteger.fromInteger(0);
         not(result.ptr, a.ptr);
         return result;
     }
 
+    /**
+     * Compute the value of the bit-wise "shift left" function of two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a << b
+     */
     public static SciInteger shl(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         shl(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Compute the value of the bit-wise "shift right" function of two SciIntegers to produce a new SciInteger instance.
+     * Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a >> b
+     */
     public static SciInteger shr(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         shr(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Set the specified bit of a SciInteger to 1. Does not modify the operand.
+     * @param a the operand
+     * @param b the bit index
+     * @return a new SciInteger instance, the result of a | (1 << b)
+     * @throws ArithmeticException if b is negative
+     */
     public static SciInteger setBit(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         setBit(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Set the specified bit of a SciInteger to 0. Does not modify the operand.
+     * @param a the operand
+     * @param b the bit index
+     * @return a new SciInteger instance, the result of a & ~(1 << b)
+     * @throws ArithmeticException if b is negative
+     */
     public static SciInteger clearBit(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         clearBit(result.ptr, a.ptr, b);
         return result;
     }
 
-    public static SciInteger testBit(SciInteger a, int b) {
-        SciInteger result = SciInteger.fromInteger(0);
-        testBit(result.ptr, a.ptr, b);
-        return result;
+    /**
+     * Compute the value of the bit at the specified index of a SciInteger. Does not modify the operand.
+     * @param a the operand
+     * @param b the bit index
+     * @return the value of the bit at index b of a
+     * @throws ArithmeticException if b is negative
+     */
+    public static boolean testBit(SciInteger a, int b) {
+        return testBit(a.ptr, b);
     }
 
+    /**
+     * Flip the value of the bit at the specified index of a SciInteger. Does not modify the operand.
+     * @param a the operand
+     * @param b the bit index
+     * @return a new SciInteger instance, the result of a ^ (1 << b)
+     * @throws ArithmeticException if b is negative
+     */
     public static SciInteger flipBit(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         flipBit(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Compute the number of bits in the binary representation of a SciInteger. Does not modify the operand.
+     * @param a the operand
+     * @return the number of bits in the binary representation of a
+     */
     public static int bitLength(SciInteger a) {
         return bitLength(a.ptr);
     }
 
+    /**
+     * Compute the number of set bits in the binary representation of a SciInteger. Does not modify the operand.
+     * Because of two complement representation, the number of set bits is equal to the number of zero bits iff
+     * a is negative and the number of one bits iff a is positive.
+     * @param a the operand
+     * @return the number of set bits in the binary representation of a
+     */
     public static int bitCount(SciInteger a) {
         return bitCount(a.ptr);
     }
 
+    /**
+     * Determine whether a SciInteger is a prime number. Does not modify the operand.
+     * Does not provide a guarantee of primality, a probabilistic test is used.
+     * @param a the operand
+     * @param certainty the number of primality-checking rounds to perform
+     * @return true if a is likely prime, false otherwise
+     * @throws ArithmeticException if certainty is negative
+     */
     public static boolean isPrime(SciInteger a, int certainty) {
         return isPrime(a.ptr, certainty);
     }
 
+    /**
+     * Find the next prime number after a SciInteger. Does not modify the operand.
+     * @param a the operand
+     * @return a new SciInteger instance, the next prime number after a
+     */
     public static SciInteger nextPrime(SciInteger a) {
         SciInteger result = SciInteger.valueOf(0);
         nextPrime(result.ptr, a.ptr);
         return result;
     }
 
+    /**
+     * Divide two SciIntegers and yield both the quotient and the remainder. Does not modify the operands.
+     * @param a the first operand
+     * @param b the second operand
+     * @return an array of SciInteger[] instances, the results of (respectively) a / b and a % b
+     */
     public static SciInteger[] divMod(SciInteger a, SciInteger b) {
         SciInteger[] tab = new SciInteger[2];
         tab[0] = SciInteger.fromInteger(0);
@@ -303,12 +568,25 @@ public final class SciInteger implements Comparable<SciInteger>, Cloneable {
         return tab;
     }
 
+    /**
+     * Compute the value of the Binomial coefficient of a SciInteger and integer to produce a new SciInteger instance.
+     * Does not modify the operand.
+     * @param a the first operand
+     * @param b the second operand
+     * @return a new SciInteger instance, the result of a choose b
+     */
     public static SciInteger binomial(SciInteger a, int b) {
         SciInteger result = SciInteger.fromInteger(0);
         binomial(result.ptr, a.ptr, b);
         return result;
     }
 
+    /**
+     * Compute the n-th Fibonacci number to produce a new SciInteger instance.
+     * @param n the index of the Fibonacci number to compute
+     * @return a new SciInteger instance, the n-th Fibonacci number
+     * @throws ArithmeticException if n is negative
+     */
     public static SciInteger fibonacci(int n) {
         SciInteger result = SciInteger.fromInteger(0);
         fibonacci(result.ptr, n);
