4d9ed11910917d85414e2e5495e3bd9f4e2c8d55
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-12-09 10:43:23 +0100

    style changes

--- java/src/main/java/palaiologos/scijava/sum/EulerMaclaurinSum.java
+++ java/src/main/java/palaiologos/scijava/sum/EulerMaclaurinSum.java
@@ -9,11 +9,12 @@ import palaiologos.scijava.util.Pair;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class EulerMaclaurinSum {
-    public static Pair<SciFloat, SciFloat> sum(MathContext mc, RealFunction f, SciFloat a, SciFloat b, Stream<SciFloat> aDiffs, Stream<SciFloat> bDiffs) {
+    public static Pair<SciFloat, SciFloat> sum(MathContext mc, RealFunction f, SciFloat a, SciFloat b, Stream<SciFloat> aDiffs, Stream<SciFloat> bDiffs, Pair<SciFloat, SciFloat> integral) {
         final int M = 10000;
         SciFloat err = SciFloat.ZERO;
         SciFloat prev = SciFloat.ZERO;
@@ -23,33 +24,25 @@ public class EulerMaclaurinSum {
 
         if(a.isInf() && a.lt(SciFloat.ZERO)) {
             ad = Stream.generate(() -> SciFloat.ZERO);
-        } else if(aDiffs != null) {
-            ad = aDiffs;
-        } else {
-            ad = Stream.generate(new Supplier<>() {
-                private int d = 0;
+        } else ad = Objects.requireNonNullElseGet(aDiffs, () -> Stream.generate(new Supplier<>() {
+            private int d = 0;
 
-                @Override
-                public SciFloat get() {
-                    return RealDifferentiation.differentiate(mc, f, a, d++);
-                }
-            });
-        }
+            @Override
+            public SciFloat get() {
+                return RealDifferentiation.differentiate(mc, f, a, d++);
+            }
+        }));
 
         if(b.isInf() && b.lt(SciFloat.ZERO)) {
             bd = Stream.generate(() -> SciFloat.ZERO);
-        } else if(bDiffs != null) {
-            bd = bDiffs;
-        } else {
-            bd = Stream.generate(new Supplier<>() {
-                private int d = 0;
+        } else bd = Objects.requireNonNullElseGet(bDiffs, () -> Stream.generate(new Supplier<>() {
+            private int d = 0;
 
-                @Override
-                public SciFloat get() {
-                    return RealDifferentiation.differentiate(mc, f, b, d++);
-                }
-            });
-        }
+            @Override
+            public SciFloat get() {
+                return RealDifferentiation.differentiate(mc, f, b, d++);
+            }
+        }));
 
         MathContext wmc = new MathContext(mc.precision() + 10, mc.roundingMode());
 
@@ -82,9 +75,14 @@ public class EulerMaclaurinSum {
             s = SciFloat.add(wmc, s, SciFloat.div(mc, f.value(mc, b), SciFloat.TWO));
         }
 
-        Pair<SciFloat, SciFloat> I = RealTanhSinhIntegrator.quad(wmc, f, new SciFloat[] { a, b });
-        s = SciFloat.add(wmc, s, I.left);
-        err = SciFloat.add(wmc, err, I.right);
+        if(integral == null) {
+            Pair<SciFloat, SciFloat> I = RealTanhSinhIntegrator.quad(wmc, f, new SciFloat[]{a, b});
+            s = SciFloat.add(wmc, s, I.left);
+            err = SciFloat.add(wmc, err, I.right);
+        } else {
+            s = SciFloat.add(wmc, s, integral.left);
+            err = SciFloat.add(wmc, err, integral.right);
+        }
 
         return new Pair<>(s, err);
     }
